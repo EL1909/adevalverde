dd5758d73678682c446dba9f9432a8bf35c59a07
el1909 <[efrain19091@gmail.com]>
2024-12-18 11:15:52 +0100

    Update Allowed hosts

--- adevalverde/settings.py
+++ adevalverde/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1",]
+ALLOWED_HOSTS = ["localhost","127.0.0.1","35.184.6.115"]
 
 
 # Application definition
